37f2c4ddac739e34bc47c295850f157811d5638c
monfrair <williamsorrentino@gmail.com>
2020-10-12 16:09:00 -0400

    add joi package to validate post

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// joi is a class so starts with a capital - joi is validation package
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 // adding express middleware to app
 app.use(express.json());
 
+// courses array
 const courses = [
 	{ id: 1, name: 'course1' },
 	{ id: 2, name: 'course2' },
@@ -20,6 +23,17 @@ app.get('/api/courses', (req, res) => {
 
 // post course to app
 app.post('/api/courses', (req, res) => {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
+	const result = Joi.validate(req.body, schema);
+	console.log(result);
+
+	// if (!req.body.name || req.body.name.length < 3) {
+	// 	// 400 bad request
+	// 	res.status(400).send('Name should be at least 3 characters');
+	// 	return;
+	// }
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name,
