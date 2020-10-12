27cab55aa505ead89222c9af471e6444751786d9
monfrair <williamsorrentino@gmail.com>
2020-10-12 15:42:13 -0400

    setting up routes in express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// adding express middleware to app
+app.use(express.json());
+
 const courses = [
 	{ id: 1, name: 'course1' },
 	{ id: 2, name: 'course2' },
@@ -14,10 +17,22 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
 	res.send(courses);
 });
-// api courses 1
+
+// post course to app
+app.post('/api/courses', (req, res) => {
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name,
+	};
+	courses.push(course);
+	res.send(course);
+});
+
+// api course: find and return course to user
 app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find((c) => c.id === parseInt(req.params.id));
 	if (!course) res.status(404).send('This course was not found');
+	res.send(course);
 });
 
 // PORT env port set to 5000 - export PORT=5000 in terminal for app
