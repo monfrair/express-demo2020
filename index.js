6372175e3f6b05e82cc73f52a1fe83083a2fc6bd
monfrair <williamsorrentino@gmail.com>
2020-10-12 16:47:27 -0400

    add joi package to validate post

--- index.js
+++ index.js
@@ -27,18 +27,37 @@ app.post('/api/courses', (req, res) => {
 		name: Joi.string().min(3).required(),
 	};
 	const result = Joi.validate(req.body, schema);
-	console.log(result);
+	// console.log(result);
 
-	// if (!req.body.name || req.body.name.length < 3) {
-	// 	// 400 bad request
-	// 	res.status(400).send('Name should be at least 3 characters');
-	// 	return;
-	// }
+	if (result.error) {
+		// 400 bad request - below sends error message to user - joi will generate error messages based on what you set.
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name,
 	};
 	courses.push(course);
+	res.send(course);
+});
+
+// updating a course with put request
+app.put('/api/courses/:id', (req, res) => {
+	// look up the course - if not existing, return 404
+	const course = courses.find((c) => c.id === parseInt(req.params.id));
+	if (!course) res.status(404).send('This course was not found');
+
+	const { error } = validateCourse(req.body); // same as result.error
+	// if invalid, return 404 - bad request
+	if (error) {
+		// 400 bad request - below sends error message to user - joi will generate error messages based on what you set.
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
+	// update course
+	course.name = req.body.name;
+	// return the updated course
 	res.send(course);
 });
 
@@ -48,6 +67,13 @@ app.get('/api/courses/:id', (req, res) => {
 	if (!course) res.status(404).send('This course was not found');
 	res.send(course);
 });
+// function to validate put reqest from user
+function validateCourse(course) {
+	const schema = {
+		name: Joi.string().min(3).required(),
+	};
+	return Joi.validate(course, schema);
+}
 
 // PORT env port set to 5000 - export PORT=5000 in terminal for app
 const port = process.env.PORT || 3000;
